f1ef293397dae144b37b8d552b09cc0741456b7a
schmicma <schmicklermarco.gmail.com>
2018-11-12 19:10:03 +0100

    Sortierung

--- main.cpp
+++ main.cpp
@@ -2,136 +2,9 @@
 #include <fstream>
 #include <string>
 
-/*int fact(int n){
-    if (n==0) {
-        return 1;
-    }
-    else {
-        return n*fact(n-1);
-    }
-}*/
-
-// struct edge
-// vector edges
-// vector verteces
-// build graph
-// check graph: step 0
-// build sorted edges
-// check cycle
-// return tree
-
 #include "unionfind.h"
 
-void einlesen() {
-    std::ifstream file;
-    std::string line;
-    file.open("C:\\Instanz1.txt");
-    if (file.is_open()){
-        while(std::getline(file, line)){
-            std::cout<<line<<std::endl;
-        }
-        file.close();
-    }
-    else {
-        std::cerr<<"Fehler beim oeffnen der Datei!"<<std::endl;
-    }
-}
-
-int main() {
-    std::ifstream file;
-    std::string line;
-    file.open("C:\\Users\\Mo\\CLionProjects\\union_find_cycle\\inst_1000000");
-//    file.open("C:\\Instanz1.txt");
-
-
-/*
-    if (file.is_open()){
-        while(std::getline(file, line)){
-            std::cout<<line<<std::endl;
-        }
-        file.close();
-    }
-    else {
-        std::cerr<<"Fehler beim oeffnen der Datei!"<<std::endl;
-    }*/
-    unsigned vertex_count;
-    file>>vertex_count;
-    std::cout<<vertex_count<<std::endl;
-
-    int n = 0;
-
-    int a,b,weight;
-    Graph graph;
-
-    while (!file.eof()) {
-        file>>a>>b>>weight;
-//        std::cout<<a<<" "<<b<<" "<<weight<<std::endl;
-        Edge edge;
-        edge.a=a;
-        edge.b=b;
-        edge.weight = weight;
-
-        graph.edges.push_back(edge);
-
-        n++;
-    }
-
-    for (unsigned i=0;i<=n;i++){
-        std::cout<<graph.edges[i].a<<" "<<graph.edges[i].b<<" "<<graph.edges[i].weight<<std::endl;
-    }
-
-
-    return 0;
-}
-    /*std::vector<int> *partitions = {-1,-1,-1,-1};
-
-   // initPartition()
-   int test1 = find(1, partitions);
-    // 1
-
-   int test2 = find(2, partitions);
-    // 2
-
-   int test3 = find(3, partitions);
-    // 2
-
-    unify(2,3, partitions);
-
-    int test4 = find(2, partitions);
-    // 2
-
-    int test5 = find(3, partitions);
-    // 2
-
-    // test 4 == test 5
-
-
-
-    //3
-    std::cout<<test1<<std::endl;
-   if (test1 == 3 ) std::cout<<"ok"<<std::endl;
-    int test2 = find(parents,0);
-    //1
-    std::cout<<test2<<std::endl;
-    if (test2 == 1 ) std::cout<<"ok"<<std::endl;
-
-}*/
-/*
-void einlesen() {
-    std::ifstream file;
-    std::string line;
-    file.open("C:\\Instanz1.txt");
-    if (file.is_open()){
-        while(std::getline(file, line)){
-            std::cout<<line<<std::endl;
-        }
-        file.close();
-    }
-    else {
-        std::cerr<<"Fehler beim oeffnen der Datei!"<<std::endl;
-    }
-}
-*/
+Graph graph;
 
 // Schritt 1: Interpretation
 // 1.Zeile lesen => Array mit Knoten erstellen von 0 bis n-1
@@ -140,7 +13,112 @@ void einlesen() {
 // die dritte Zahl als Gewicht der passenden Kante zuordnen
 // m:= Anzahl der Kanten
 // m-dimensionales Array der Laenge 3 a[i,0], a[i,1] Kante, a[i,2] Kosten fuer i=0,...,m-1
-// Alternative: Vector-Klasse??
+void einlesen(char *dateiname);
+
+bool check_graph();
+
 // Schritt 2: Sortierung: Sortiere Kanten nach Gewicht mit qsort bzw std::sort
+void sortieren();
+
 // Schritt 3: Union Find Kreise, Hinzufügen, Zhk verwalten
+void union_find();
+
 // Schritt 4: Ausgabe
+void ausgeben(Graph &result);
+
+void union_find();
+
+void einlesen(char *dateiname) {
+    std::ifstream file;
+
+    file.open(dateiname);
+
+    if (file.is_open()){
+        unsigned vertex_count;
+        file>>vertex_count;
+
+        graph.vertex_count = vertex_count;
+
+        unsigned n = 0;
+
+        Edge edge;
+        int a,b,weight;
+
+        while (!file.eof()) {
+            file>>a>>b>>weight;
+
+            edge.a=a;
+            edge.b=b;
+            edge.weight = weight;
+
+            graph.edges.push_back(edge);
+
+            n++;
+        }
+
+        graph.edge_count = n;
+
+        file.close();
+    }
+    else {
+        std::cerr<<"Fehler beim oeffnen der Datei!"<<std::endl;
+    }
+}
+
+struct graph_weight
+{
+    inline bool operator() (const Edge& struct1, const Edge& struct2)
+    {
+        return (struct1.weight < struct2.weight);
+    }
+};
+
+// todo prüfen, ob alle Knoten verbunden sind
+
+bool check_graph() {
+    return true;
+}
+
+void sortieren() {
+    std::sort(graph.edges.begin(), graph.edges.end(), graph_weight());
+}
+
+int main(int argc,  char** argv) {
+    if (argc != 2) {
+        std::cout<<"Bitte geben Sie als Argument eine Datei an."<<std::endl;
+        return 0;
+    }
+
+    einlesen(argv[1]);
+
+    if (!check_graph()) {
+        std::cout<<"Die Knoten sind nicht alle erreichbar."<<std::endl;
+    }
+
+    sortieren();
+
+    ausgeben(graph);
+
+    union_find();
+
+    return 0;
+}
+
+void union_find() {
+
+}
+
+void ausgeben(Graph &result) {
+    int weight = 0;
+    for (unsigned i=0; i <= result.vertex_count; i++){
+        weight += result.edges[i].weight;
+    }
+
+    std::cout<<"The following is a minimum weight spanning tree with weight "<< weight << std::endl;
+    std::cout<<"The graph contains vertices 0,...," << result.vertex_count - 1 << " and the following edges:"<<std::endl;
+
+    for (unsigned i=0; i <= result.vertex_count; i++){
+        std::cout<<"{" << result.edges[i].a << "," << result.edges[i].b << "} with weight " << result.edges[i].weight << std::endl;
+    }
+}
+
